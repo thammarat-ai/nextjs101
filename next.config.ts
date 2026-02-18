e3a1d7066f8c995b9dbc48867a320ae406eef635
thammarat-ai <thammarat.th@up.ac.th>
2026-02-18 21:14:59 +0700

    chapter-10 done

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* การตั้งค่าประสิทธิภาพอื่นๆ */
+  output: "standalone",
   reactCompiler: true,
   cacheComponents: true,
   images: {
